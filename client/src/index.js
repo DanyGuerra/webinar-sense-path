5fe8ae1ab500d7a2103246b9fa0e13b18aaadb42
DanyGuerra <luis_daniel.grra15@hotmail.com>
2022-05-24 14:03:31 -0500

    Global styles agregados

--- client/src/index.js
+++ client/src/index.js
@@ -2,45 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 import reportWebVitals from "./reportWebVitals";
-// import { createGlobalStyle } from "styled-components";
-
-// const GlobalStyle = createGlobalStyle`
-//   @import url("https://fonts.googleapis.com/css2?family=Sen:wght@400;700;800&display=swap");
-//   body {
-//     margin:0;
-//     padding: 0;
-//     font-family: "Sen";
-//     font-style: normal;
-//     font-weight: 400;
-//     font-size: 18px;
-
-//   }
-
-//   ::-webkit-scrollbar {
-//       width: 0.7em;
-//     }
-
-//     ::-webkit-scrollbar-track {
-//       background-color: #e5e5e5;
-//     }
-
-//     ::-webkit-scrollbar-thumb {
-//       background: #c4c4c4;
-//       border-radius: 10px;
-//     }
-//     ::-webkit-scrollbar-thumb:hover {
-//       background-color: rgba(0, 0, 0, 0.25);
-//     }
-
-//     ::-webkit-scrollbar-thumb:active {
-//       background-color: rgba(0, 0, 0, 0.4);
-//     }
-
-//   *{
-//     box-sizing: border-box;
-//   }
-
-// `;
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
